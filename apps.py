e96ea4acf89e5b3f35aeeb63d896009b6b5b1242
Bhawana <bhawanazende@gmail.com>
2024-09-27 12:19:06 -0700

    hey

--- apps.py
+++ apps.py
@@ -4,5 +4,5 @@ st.title("Welcome App")
 name = st.text_input("What is your name?")
 if name:
     st.write(f"Hello, {name}! Welcome to Streamlit!")
-    st.write(f"Hello, {name}! Welcome to Streamlit!")
+    st.write(f"Hell, {name}! Welcome to Streamlit!")
 
