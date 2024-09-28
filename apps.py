70e132a940188d49e693a5bb81ac442cf5fcdb9a
Bhawana <bhawanazende@gmail.com>
2024-09-27 17:35:33 -0700

    simple changes

--- apps.py
+++ apps.py
@@ -1,19 +1,44 @@
 import streamlit as st
 
 # Title of the app
-st.title("Welcome App")
+st.title("Welcome to the Fun Zone! 🎉")
 
-# Asking for the user's name
-name = st.text_input("Hi! What's your name?")
+# Greeting message
+st.write("🎉 Hi Xinlue! Are you ready for a super awesome game? 🎈")
 
-# If the user enters their name
-if name:
-    st.write(f"Hello, {name}! Welcome to Streamlit!")
+# Silly questions and their "correct" answers
+questions = {
+    "If Xinlue could be a vegetable, what would you be? (And why?) 🥦": "Carrot",
+    "If you could have any animal as a pet, which animal would you choose? 🎩": "Red Panda",
+    "whats xinlues famous sentence 💃🕺": "Play with me ",
+    "If you were a character in a cartoon, what would your catchphrase be? 📺": "The Big Ear Tutu!"
+}
 
-    # Food options
-    food_options = ["Pizza", "Burger", "Pasta"]
-    selected_food = st.selectbox("Please select your food:", food_options)
+# Initialize session state to track questions
+if 'question_index' not in st.session_state:
+    st.session_state.question_index = 0
 
-    # Confirmation message
-    st.write(f"You have selected: {selected_food}")
-    st.write("Your food is going to be ready in 15 minutes!")
+# Function to display the current question
+def display_question():
+    question = list(questions.keys())[st.session_state.question_index]
+    answer = st.text_input(question)
+
+    if st.button("Submit"):
+        if answer:
+            correct_answer = questions[question]
+            if answer.lower() == correct_answer.lower():
+                st.write("🎉 Right! You totally nailed it! 🏆")
+            else:
+                st.write(f"Oops! That's not quite right. The correct answer is: **{correct_answer}** 😄")
+            
+            # Move to the next question
+            st.session_state.question_index += 1
+
+# Check if there are more questions to ask
+if st.session_state.question_index < len(questions):
+    display_question()
+else:
+    st.write("Bhawana loves you, silly boy! 💖 Thanks for playing! Remember, the world needs more fun and laughter!")
+    # Reset for another round if desired
+    if st.button("Play Again"):
+        st.session_state.question_index = 0
